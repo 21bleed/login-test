11382fabce0fc053a181ce11fcea8e021f2bcd3d
alexander <alexander.nilson@yahoo.se>
2025-08-21 13:24:26 +0200

    shrek

---
+++ www/public/shrek.php
@@ -0,0 +1,11 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SHREK</title>
+ </head>
+ <body>
+    <img src="https://i.pinimg.com/originals/02/6c/5d/026c5dab9176700ffd06beb7db7da674.gif" alt="shrek gif" style="width: 100%; height: auto;">
+ </body>
+ </html>
